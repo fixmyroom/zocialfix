14347e3001eade2b2a327d0b6959585fac08a2f1
fixmyroom <aryalzsattvik@gmail.com>
2025-08-14 21:28:49 +0545

    Move firebase-config.js from public to js

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAB5KK4gMMwCAzmxjzTRAd0gK-gzpzbzmw",
+  authDomain: "fixmyroomwithzocial.firebaseapp.com",
+  databaseURL: "https://fixmyroomwithzocial-default-rtdb.firebaseio.com",
+  projectId: "fixmyroomwithzocial",
+  storageBucket: "fixmyroomwithzocial.firebasestorage.app",
+  messagingSenderId: "594671909340",
+  appId: "1:594671909340:web:9a9feedefed205a50ef719"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
